20634e810776b818a50976a61430382b566fdee5
Felipe Alves <felipealvesferreira05@outlook.com>
2022-03-13 20:49:28 -0300

    FirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TarefasModule } from './tarefas';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    TarefasModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {TarefaService, TarefaConcluidaDirective} from './shared';
+import { ListarTarefaComponent } from './listar';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+
+
+
+@NgModule({
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective,
+  ],
+  providers:[
+    TarefaService
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ]
+})
+export class TarefasModule { }
